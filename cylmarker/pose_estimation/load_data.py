4a95154cfc8501d79925f4babdb8f40fc290d590
Cartucho <joaocartuchoo@gmail.com>
2021-01-22 08:51:42 +0000

    load camera calibration data

--- cylmarker/pose_estimation/load_data.py
+++ cylmarker/pose_estimation/load_data.py
@@ -1,2 +1,32 @@
-def load_data():
-    print('test!')
+import errno
+import os
+import yaml
+
+
+def is_path_dir(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), string)
+
+
+def is_path_file(string):
+    if os.path.isfile(string):
+        return string
+    else:
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), string)
+
+
+
+def load_cam_calib_data(path):
+    if is_path_file(path):
+        with open(path) as f_tmp:
+            return yaml.load(f_tmp, Loader=yaml.FullLoader)
+
+
+def load_data(data_dir):
+    # Load camera intrinsic matrix and distortion coefficients
+    cam_calib_file = os.path.join(data_dir, 'camera_calibration.yaml')
+    cam_calib_data = load_cam_calib_data(cam_calib_file)
+
+    return cam_calib_data
--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import argparse
+import os
+
 from cylmarker.pose_estimation import load_data
 
-load_data.load_data()
+
+def main():
+    # Get path to the data dir
+    parser = argparse.ArgumentParser(description='This program calculates the 6DoF pose of a cylindrical marker.')
+    parser.add_argument('--path', type=load_data.is_path_dir, default='data', help= 'Path to folder with (a) intrinsic camera parameters and (b) marker pattern data.')
+    args = parser.parse_args()
+
+    # Load data
+    cam_data = load_data.load_data(args.path)
+    print(cam_data)
+
+if __name__ == "__main__":
+    main()
+
