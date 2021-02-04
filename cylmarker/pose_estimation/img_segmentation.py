2afb77ae5678eaf75c2ac1ba5a100eeaf4c90e2a
Cartucho <joaocartuchoo@gmail.com>
2021-02-04 10:03:01 +0000

    adding functions for visualization/debuging and fixing foreground segmentation

--- cylmarker/pose_estimation/img_segmentation.py
+++ cylmarker/pose_estimation/img_segmentation.py
@@ -1,5 +1,8 @@
 import cv2 as cv
 import numpy as np
+import matplotlib
+
+from matplotlib import pyplot as plt # For showing the histograms of the segmented marker
 
 
 def get_hsv_lower_and_upper(h, h_margin, s_min, s_max, v_min, v_max):
@@ -66,42 +69,106 @@ def get_marker_background(im_hsv, config_file_data):
 
     # Erode mask (given that we already have the biggest green contour)
     kernel = np.ones((3, 3), np.uint8)
-    mask_marker_bg = cv.erode(mask_marker_bg, kernel, iterations = 3)
+    mask_marker_bg = cv.erode(mask_marker_bg, kernel, iterations = 1)
 
     return mask_marker_bg
 
 
 def get_marker_foreground(im_hsv, mask_marker_bg, config_file_data):
-    h = 90#config_file_data['marker_bg_hue']
-    h_margin = 90#config_file_data['marker_bg_hue_margin']
-    # Load foreground HSV data
-    s_min = config_file_data['marker_fg_s_min']
-    s_max = config_file_data['marker_fg_s_max']
-    v_min = config_file_data['marker_fg_v_min']
-    v_max = config_file_data['marker_fg_v_max']
+    # We will distinguish the foreground and the background using the V channel
+    #  the intuition is that the darker parts of the marker should correspond to the keypoints
+    th = cv.adaptiveThreshold(im_hsv[:,:,2], 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                 cv.THRESH_BINARY_INV, 17, 2)
+    mask_fg_colour = cv.bitwise_and(th, th, mask=mask_marker_bg)
+    #cv.imshow('test', th)
+    #cv.waitKey(0)
 
-    # Get lower and upper values
+    """
+    # First, we get the darkest region of the HSV
+    h = 90
+    h_margin = 90 # h = 90 and h_margin = 90 will cover all the H's
+    s_min = 0
+    s_max = 255 # cover all the possible S's too
+    v_min = 0
+    v_max = int(0.15*255) # TODO: should be coming from the config file
+
+    ## Get lower and upper values
     lower, upper = get_hsv_lower_and_upper(h, h_margin, s_min, s_max, v_min, v_max)
     mask_fg_colour = get_hsv_mask(im_hsv, lower, upper)
     # Intersect mask with background mask
     mask_fg_colour = cv.bitwise_and(mask_fg_colour, mask_fg_colour, mask=mask_marker_bg)
 
+    # Then we get the "black-greenish" regions
+    h = config_file_data['marker_bg_hue']
+    h_margin = config_file_data['marker_bg_hue_margin']
+    s_min = 0 #25 #config_file_data['marker_fg_s_min'] # try with 0 too
+    s_max = 255 #config_file_data['marker_fg_s_max']
+    v_min = v_max
+    v_max = v_max + int(0.25*255)
+    if v_max > 255:
+        v_max = 255
 
-    #cv.imshow('test_fg', mask_fg_colour) # TODO: remove
+    lower, upper = get_hsv_lower_and_upper(h, h_margin, s_min, s_max, v_min, v_max)
+    mask_fg_colour_2 = get_hsv_mask(im_hsv, lower, upper)
+    #cv.imshow('1', mask_fg_colour)
+    #cv.imshow('2', mask_fg_colour_2)
+    # Intersect mask with background mask
+    mask_fg_colour = cv.bitwise_or(mask_fg_colour, mask_fg_colour_2, mask=mask_marker_bg)
+    """
 
     return mask_fg_colour
+
+
+def show_hsv_image(im_hsv):
+    im_hsv_copy = cv.cvtColor(im_hsv, cv.COLOR_BGR2RGB)
+    cv.imshow('image HSV | Red: H, Green:S, Blue:V', im_hsv_copy)
+    cv.waitKey(0)
+
+
+def show_marker_histogram(im_hsv, mask_marker_bg):
+    color = ('r','g','b')
+    label = ('H', 'S', 'V')
+    plt.clf()
+
+    # HSV
+    plot_just_v = False
+    for i, (col, lab) in enumerate(zip(color, label)):
+        if plot_just_v:
+            if i != 2:
+                continue
+        histr = cv.calcHist([im_hsv], [i], mask_marker_bg, [256], [0,256])
+        plt.plot(histr.copy(), color = col, label = lab)
+        plt.xlim([0,256])
+    plt.legend(loc="upper right")
+
+    #plt.show()
+    # Convert plot to numpy image so that I can show with OpenCV
+    fig = plt.gcf()
+    fig.canvas.draw()
+    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    data_bgr = cv.cvtColor(data, cv.COLOR_RGB2BGR)
+    cv.imshow("Marker histograms", data_bgr)
+    cv.waitKey(0)
+
+
+def show_marker_histogram_gray(im, mask_marker_bg):
+    im_gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+    cv.imshow('test', im_gray)
+    cv.waitKey(0)
 
 
 def marker_segmentation(im, config_file_data):
     #print(config_file_data)
     # Segment the marker assuming that it has a unique colour
     im_hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
-    #cv.imshow('test', im_hsv) # TODO: remove
-    #cv.waitKey(0)
+    #show_hsv_image(im_hsv)
     mask_marker_bg = get_marker_background(im_hsv, config_file_data)
     #marker_bg = cv.bitwise_and(im, im, mask=mask_marker_bg)
     #cv.imshow('marker_bg', marker_bg) # TODO: remove
     marker_bg_hsv = cv.bitwise_and(im_hsv, im_hsv, mask=mask_marker_bg)
+    #show_marker_histogram(im_hsv, mask_marker_bg)
+    #show_marker_histogram_gray(im, mask_marker_bg)
     mask_marker_fg = get_marker_foreground(marker_bg_hsv, mask_marker_bg, config_file_data)
 
     return mask_marker_bg, mask_marker_fg
--- cylmarker/pose_estimation/pose_estimation.py
+++ cylmarker/pose_estimation/pose_estimation.py
@@ -11,6 +11,56 @@ def check_image(im, im_path):
         exit()
 
 
+def draw_sgmntd_bg_and_fg(im, mask_marker_bg, mask_marker_fg):
+    im_copy = im.copy()
+    alpha = 0.4
+    # First we show the background part only
+    mask_marker_bg = cv.subtract(mask_marker_bg, mask_marker_fg);
+    mask_bg_blue = np.zeros_like(im_copy)
+    mask_bg_blue[:,:,0] = mask_marker_bg
+    im_copy = cv.addWeighted(im_copy, 1.0, mask_bg_blue, alpha, 0)
+    # Then we show the foreground part
+    alpha = 0.7
+    mask_fg_red = np.zeros_like(im_copy)
+    mask_fg_red[:,:,2] = mask_marker_fg
+    im_copy = cv.addWeighted(im_copy, 1.0, mask_fg_red, alpha, 0)
+    cv.imshow("Segmentation | Blue: background | Red: foreground", im_copy)
+    cv.waitKey(0)
+
+
+def draw_contours_and_lines(im, pttrn):
+    red = (0, 0, 255)
+    green = (0, 255, 0)
+    for sqnc in pttrn.list_sqnc:
+        if sqnc.sqnc_id != -1:
+            # draw line between first and last kpt
+            u_0, v_0 = sqnc.list_kpts[0].get_centre_uv()
+            u_1, v_1 = sqnc.list_kpts[-1].get_centre_uv()
+            im = cv.line(im, (int(u_0), int(v_0)), (int(u_1), int(v_1)), green, 1)
+            # draw contours
+            for kpt in sqnc.list_kpts:
+                cntr = kpt.cntr
+                cv.drawContours(im, [cntr], -1, red, 1)
+    cv.imshow("image", im)
+    cv.waitKey(0)
+
+
+def draw_axis(im, rvecs, tvecs, cam_matrix, dist_coeff):
+    #print(cam_matrix)
+    #print(np.transpose(tvecs))
+    axis = np.float32([[0, 0, 0], [3,0,0], [0,3,0], [0,0,3]]).reshape(-1,3)
+    #print(axis)
+    imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, cam_matrix, dist_coeff)
+    print(imgpts)
+    frame_centre = tuple(imgpts[0].ravel())
+    im = cv.line(im, frame_centre, tuple(imgpts[1].ravel()), (255,0,0), 5)
+    im = cv.line(im, frame_centre, tuple(imgpts[2].ravel()), (0,255,0), 5)
+    im = cv.line(im, frame_centre, tuple(imgpts[3].ravel()), (0,0,255), 5)
+    cv.imshow("image", im)
+    cv.waitKey(0)
+    #exit()
+
+
 def estimate_poses(cam_calib_data, config_file_data, data_pttrn, data_marker):
     # Load data needed to estimate pose
     min_detected_lines = config_file_data['min_detected_lines']
@@ -29,11 +79,17 @@ def estimate_poses(cam_calib_data, config_file_data, data_pttrn, data_marker):
         check_image(im, im_path) # check if image was sucessfully read
         # Segment the marker
         mask_marker_bg, mask_marker_fg = img_segmentation.marker_segmentation(im, config_file_data)
+        # Draw segmented background and foreground
+        draw_sgmntd_bg_and_fg(im, mask_marker_bg, mask_marker_fg)
         # Find keypoints
         pttrn = keypoints.find_keypoints(mask_marker_fg, min_detected_lines, max_ang_diff, sequence_length, data_pttrn, data_marker)
         # Estimate pose
         if pttrn is not None:
+            # Draw contours and lines (for visualization)
+            draw_contours_and_lines(im, pttrn)
             pnts_3d_object, pnts_2d_image = pttrn.get_data_for_pnp_solver()
-            # cv2.SOLVEPNP_EPNP is faster than ITERATIVE
+            # Estomate pose using the PnPRansac (cv.SOLVEPNP_EPNP is faster than cv.ITERATIVE)
             retval, rvec_pred, tvec_pred, inliers = cv.solvePnPRansac(pnts_3d_object, pnts_2d_image, cam_matrix, dist_coeff, None, None, False, 1000, 3.0, 0.9999, None, cv.SOLVEPNP_EPNP)
+            # Draw axis
+            draw_axis(im, rvec_pred, tvec_pred, cam_matrix, dist_coeff)
             # TODO: Validate solution
